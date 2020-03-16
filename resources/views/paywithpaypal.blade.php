ae8e9088da7228235d502bfd8c023110d3f10b95
weesiangkoh <weesiang1234@gmail.com>
2020-03-16 10:59:36 +0800

    first commit

--- resources/views/paywithpaypal.blade.php
+++ resources/views/paywithpaypal.blade.php
@@ -15,7 +15,7 @@
 
         @if ($message = Session::get('error'))
         <div class="w3-panel w3-red w3-display-container">
-            <span onclick="this.parentElement.style.display='none'"
+            <span onclick="this.parentElements.style.display='none'"
     				class="w3-button w3-red w3-large w3-display-topright">&times;</span>
             <p>{!! $message !!}</p>
         </div>
